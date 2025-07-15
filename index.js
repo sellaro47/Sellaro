0e23c1aacf8f7b3b9df891726f7148af40c7e65b
sellaro47 <bazarkaz11@gmail.com>
2025-07-15 05:17:05 +0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
-      <h1>Welcome to Sellaro</h1>
-      <p>This is your AI-powered lead gen dashboard.</p>
-    </div>
-  )
-}
